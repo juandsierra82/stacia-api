c9bea62c24d9638fae64f421639e8013671fa1b8
juan sierra <juandsierra82@gmail.com>
2024-09-07 18:13:35 -0400

    psql schema set up

--- prisma/migrations/20240907221223_init/migration.sql
+++ prisma/migrations/20240907221223_init/migration.sql
@@ -2,7 +2,13 @@
 CREATE TYPE "OwnerRole" AS ENUM ('MEMBER', 'PRESIDENT', 'TREASURER', 'SECRETARY', 'BOARD_ASSOCIATE');
 
 -- CreateEnum
-CREATE TYPE "FeeType" AS ENUM ('MONTHLY', 'SPECIAL_ASSESSMENT', 'THIRD_PARTY', 'RULES_VIOLATION', 'INTEREST_OWED', 'REPARATION', 'OTHER');
+CREATE TYPE "CommitteeType" AS ENUM ('BOARD', 'GRIEVANCE', 'BUDGET', 'SPECIAL', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "DocumentType" AS ENUM ('PICTURE', 'VIDEO', 'TEXT');
+
+-- CreateEnum
+CREATE TYPE "FeeType" AS ENUM ('MONTHLY', 'THIRD_PARTY', 'RULES_VIOLATION', 'INTEREST_OWED', 'REPARATION', 'APPLIED_TO_RESIDENT', 'OTHER');
 
 -- CreateEnum
 CREATE TYPE "CommunicationMethod" AS ENUM ('EMAIL', 'CHAT', 'TEXT', 'CALL');
@@ -15,8 +21,28 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "admin" BOOLEAN NOT NULL DEFAULT false,
+    "avatarUrl" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Building" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "address1" TEXT NOT NULL,
+    "address2" TEXT,
+    "name" TEXT NOT NULL,
+    "dateFounded" TIMESTAMP(3),
+    "city" TEXT NOT NULL,
+    "municipality" TEXT NOT NULL,
+    "country" TEXT NOT NULL DEFAULT 'US',
+    "postalCode" TEXT NOT NULL,
+    "avatarUrl" TEXT,
+    "description" TEXT,
+
+    CONSTRAINT "Building_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -25,9 +51,34 @@ CREATE TABLE "Unit" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "address" TEXT NOT NULL,
+    "buildingId" INTEGER NOT NULL,
     "buildingPercentage" DOUBLE PRECISION,
 
     CONSTRAINT "Unit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Parking" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "location" TEXT NOT NULL,
+    "unitId" INTEGER NOT NULL,
+
+    CONSTRAINT "Parking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Car" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "make" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "licensePlate" TEXT NOT NULL,
+    "parkingId" INTEGER NOT NULL,
+
+    CONSTRAINT "Car_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -35,13 +86,37 @@ CREATE TABLE "Owner" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "buildingId" INTEGER NOT NULL,
     "role" "OwnerRole" NOT NULL DEFAULT 'MEMBER',
     "name" TEXT NOT NULL,
+    "birthDate" TIMESTAMP(3),
+    "avatarUrl" TEXT,
     "email" TEXT,
     "phone" TEXT,
     "partOfBoard" BOOLEAN NOT NULL DEFAULT false,
+    "proxies" TEXT[],
 
     CONSTRAINT "Owner_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ContactCard" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "contactName" TEXT NOT NULL,
+    "address1" TEXT,
+    "address2" TEXT,
+    "city" TEXT,
+    "municipality" TEXT,
+    "muncipalCode" TEXT,
+    "country" TEXT,
+    "email" TEXT,
+    "phone" TEXT,
+    "primary" BOOLEAN NOT NULL DEFAULT false,
+    "ownerId" INTEGER,
+
+    CONSTRAINT "ContactCard_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -55,6 +130,7 @@ CREATE TABLE "Resident" (
     "phone" TEXT,
     "isOwner" BOOLEAN NOT NULL DEFAULT true,
     "landLordId" INTEGER,
+    "avatarUrl" TEXT,
 
     CONSTRAINT "Resident_pkey" PRIMARY KEY ("id")
 );
@@ -65,12 +141,15 @@ CREATE TABLE "Vendor" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "name" TEXT NOT NULL,
+    "avatarUrl" TEXT,
     "mainContact" TEXT NOT NULL,
     "email" TEXT,
     "phone" TEXT,
     "address" TEXT,
+    "preferred" BOOLEAN NOT NULL DEFAULT false,
     "projectId" INTEGER,
     "budgetItemId" INTEGER,
+    "buildingId" INTEGER,
 
     CONSTRAINT "Vendor_pkey" PRIMARY KEY ("id")
 );
@@ -91,6 +170,18 @@ CREATE TABLE "Invoice" (
 );
 
 -- CreateTable
+CREATE TABLE "Committee" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "type" "CommitteeType" NOT NULL DEFAULT 'BOARD',
+    "description" TEXT,
+    "admin" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Committee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Document" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -102,9 +193,11 @@ CREATE TABLE "Document" (
     "specialAssessmentId" INTEGER,
     "url" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "type" "DocumentType" NOT NULL DEFAULT 'TEXT',
     "description" TEXT,
     "budgetSheetId" INTEGER,
     "budgetId" INTEGER,
+    "buildingId" INTEGER,
 
     CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
 );
@@ -120,6 +213,7 @@ CREATE TABLE "Project" (
     "title" TEXT NOT NULL,
     "description" TEXT,
     "budgetedCost" DOUBLE PRECISION,
+    "buildingId" INTEGER NOT NULL,
 
     CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
 );
@@ -155,6 +249,7 @@ CREATE TABLE "Communication" (
     "residentId" INTEGER,
     "method" "CommunicationMethod" NOT NULL DEFAULT 'EMAIL',
     "specialAssessmentId" INTEGER,
+    "buildingId" INTEGER,
 
     CONSTRAINT "Communication_pkey" PRIMARY KEY ("id")
 );
@@ -164,7 +259,10 @@ CREATE TABLE "Budget" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "buildingId" INTEGER NOT NULL,
     "year" INTEGER NOT NULL,
+    "fiscalStart" TIMESTAMP(3) NOT NULL,
+    "fiscalEnd" TIMESTAMP(3) NOT NULL,
     "allocatedExpense" DOUBLE PRECISION,
     "currentExpense" DOUBLE PRECISION,
     "receivedIncome" DOUBLE PRECISION,
@@ -213,7 +311,7 @@ CREATE TABLE "Payment" (
     "amount" DOUBLE PRECISION,
     "feeId" INTEGER NOT NULL,
     "notes" TEXT[],
-    "payedBy" TEXT NOT NULL,
+    "paidBy" TEXT NOT NULL,
 
     CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
 );
@@ -223,6 +321,7 @@ CREATE TABLE "SpecialAssessment" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "buildingId" INTEGER NOT NULL,
     "approvedOn" TIMESTAMP(3) NOT NULL,
     "paymentSchedule" TIMESTAMP(3)[],
     "total" DOUBLE PRECISION,
@@ -232,13 +331,61 @@ CREATE TABLE "SpecialAssessment" (
 );
 
 -- CreateTable
+CREATE TABLE "_ParkingToResident" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "_OwnerToUnit" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL
 );
 
 -- CreateTable
+CREATE TABLE "_OwnerToVendor" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_OwnerToProject" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_OwnerToSpecialAssessment" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_InvoiceToOwner" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_CommitteeToOwner" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "_TaskGraph" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_BudgetToOwner" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_FeeToOwner" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL
 );
@@ -256,6 +403,18 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE INDEX "User_email_idx" ON "User"("email");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Building_address1_key" ON "Building"("address1");
+
+-- CreateIndex
+CREATE INDEX "Building_name_address1_idx" ON "Building"("name", "address1");
+
+-- CreateIndex
+CREATE INDEX "Building_address1_idx" ON "Building"("address1");
+
+-- CreateIndex
+CREATE INDEX "Unit_address_buildingId_idx" ON "Unit"("address", "buildingId");
+
+-- CreateIndex
 CREATE INDEX "Owner_email_idx" ON "Owner"("email");
 
 -- CreateIndex
@@ -268,10 +427,55 @@ CREATE INDEX "Document_url_idx" ON "Document"("url");
 CREATE INDEX "Task_projectId_idx" ON "Task"("projectId");
 
 -- CreateIndex
+CREATE INDEX "Fee_owedById_idx" ON "Fee"("owedById");
+
+-- CreateIndex
+CREATE INDEX "Payment_paidBy_idx" ON "Payment"("paidBy");
+
+-- CreateIndex
+CREATE INDEX "Payment_feeId_idx" ON "Payment"("feeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ParkingToResident_AB_unique" ON "_ParkingToResident"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ParkingToResident_B_index" ON "_ParkingToResident"("B");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_OwnerToUnit_AB_unique" ON "_OwnerToUnit"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_OwnerToUnit_B_index" ON "_OwnerToUnit"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_OwnerToVendor_AB_unique" ON "_OwnerToVendor"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_OwnerToVendor_B_index" ON "_OwnerToVendor"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_OwnerToProject_AB_unique" ON "_OwnerToProject"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_OwnerToProject_B_index" ON "_OwnerToProject"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_OwnerToSpecialAssessment_AB_unique" ON "_OwnerToSpecialAssessment"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_OwnerToSpecialAssessment_B_index" ON "_OwnerToSpecialAssessment"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_InvoiceToOwner_AB_unique" ON "_InvoiceToOwner"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_InvoiceToOwner_B_index" ON "_InvoiceToOwner"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CommitteeToOwner_AB_unique" ON "_CommitteeToOwner"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CommitteeToOwner_B_index" ON "_CommitteeToOwner"("B");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_TaskGraph_AB_unique" ON "_TaskGraph"("A", "B");
@@ -280,10 +484,37 @@ CREATE UNIQUE INDEX "_TaskGraph_AB_unique" ON "_TaskGraph"("A", "B");
 CREATE INDEX "_TaskGraph_B_index" ON "_TaskGraph"("B");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "_BudgetToOwner_AB_unique" ON "_BudgetToOwner"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_BudgetToOwner_B_index" ON "_BudgetToOwner"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_FeeToOwner_AB_unique" ON "_FeeToOwner"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_FeeToOwner_B_index" ON "_FeeToOwner"("B");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_SpecialAssessmentToUnit_AB_unique" ON "_SpecialAssessmentToUnit"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_SpecialAssessmentToUnit_B_index" ON "_SpecialAssessmentToUnit"("B");
+
+-- AddForeignKey
+ALTER TABLE "Unit" ADD CONSTRAINT "Unit_buildingId_fkey" FOREIGN KEY ("buildingId") REFERENCES "Building"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Parking" ADD CONSTRAINT "Parking_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES "Unit"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Car" ADD CONSTRAINT "Car_parkingId_fkey" FOREIGN KEY ("parkingId") REFERENCES "Parking"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Owner" ADD CONSTRAINT "Owner_buildingId_fkey" FOREIGN KEY ("buildingId") REFERENCES "Building"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ContactCard" ADD CONSTRAINT "ContactCard_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Owner"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Resident" ADD CONSTRAINT "Resident_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES "Unit"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -296,6 +527,9 @@ ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_projectId_fkey" FOREIGN KEY ("projec
 
 -- AddForeignKey
 ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_budgetItemId_fkey" FOREIGN KEY ("budgetItemId") REFERENCES "BudgetItem"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vendor" ADD CONSTRAINT "Vendor_buildingId_fkey" FOREIGN KEY ("buildingId") REFERENCES "Building"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_vendorId_fkey" FOREIGN KEY ("vendorId") REFERENCES "Vendor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -328,6 +562,12 @@ ALTER TABLE "Document" ADD CONSTRAINT "Document_budgetSheetId_fkey" FOREIGN KEY 
 ALTER TABLE "Document" ADD CONSTRAINT "Document_budgetId_fkey" FOREIGN KEY ("budgetId") REFERENCES "Budget"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Document" ADD CONSTRAINT "Document_buildingId_fkey" FOREIGN KEY ("buildingId") REFERENCES "Building"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_buildingId_fkey" FOREIGN KEY ("buildingId") REFERENCES "Building"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Task" ADD CONSTRAINT "Task_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -346,6 +586,12 @@ ALTER TABLE "Communication" ADD CONSTRAINT "Communication_residentId_fkey" FOREI
 ALTER TABLE "Communication" ADD CONSTRAINT "Communication_specialAssessmentId_fkey" FOREIGN KEY ("specialAssessmentId") REFERENCES "SpecialAssessment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Communication" ADD CONSTRAINT "Communication_buildingId_fkey" FOREIGN KEY ("buildingId") REFERENCES "Building"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Budget" ADD CONSTRAINT "Budget_buildingId_fkey" FOREIGN KEY ("buildingId") REFERENCES "Building"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "BudgetItem" ADD CONSTRAINT "BudgetItem_budgetId_fkey" FOREIGN KEY ("budgetId") REFERENCES "Budget"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -355,16 +601,67 @@ ALTER TABLE "Fee" ADD CONSTRAINT "Fee_owedById_fkey" FOREIGN KEY ("owedById") RE
 ALTER TABLE "Payment" ADD CONSTRAINT "Payment_feeId_fkey" FOREIGN KEY ("feeId") REFERENCES "Fee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "SpecialAssessment" ADD CONSTRAINT "SpecialAssessment_buildingId_fkey" FOREIGN KEY ("buildingId") REFERENCES "Building"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ParkingToResident" ADD CONSTRAINT "_ParkingToResident_A_fkey" FOREIGN KEY ("A") REFERENCES "Parking"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ParkingToResident" ADD CONSTRAINT "_ParkingToResident_B_fkey" FOREIGN KEY ("B") REFERENCES "Resident"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "_OwnerToUnit" ADD CONSTRAINT "_OwnerToUnit_A_fkey" FOREIGN KEY ("A") REFERENCES "Owner"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_OwnerToUnit" ADD CONSTRAINT "_OwnerToUnit_B_fkey" FOREIGN KEY ("B") REFERENCES "Unit"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "_OwnerToVendor" ADD CONSTRAINT "_OwnerToVendor_A_fkey" FOREIGN KEY ("A") REFERENCES "Owner"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OwnerToVendor" ADD CONSTRAINT "_OwnerToVendor_B_fkey" FOREIGN KEY ("B") REFERENCES "Vendor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OwnerToProject" ADD CONSTRAINT "_OwnerToProject_A_fkey" FOREIGN KEY ("A") REFERENCES "Owner"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OwnerToProject" ADD CONSTRAINT "_OwnerToProject_B_fkey" FOREIGN KEY ("B") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OwnerToSpecialAssessment" ADD CONSTRAINT "_OwnerToSpecialAssessment_A_fkey" FOREIGN KEY ("A") REFERENCES "Owner"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OwnerToSpecialAssessment" ADD CONSTRAINT "_OwnerToSpecialAssessment_B_fkey" FOREIGN KEY ("B") REFERENCES "SpecialAssessment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_InvoiceToOwner" ADD CONSTRAINT "_InvoiceToOwner_A_fkey" FOREIGN KEY ("A") REFERENCES "Invoice"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_InvoiceToOwner" ADD CONSTRAINT "_InvoiceToOwner_B_fkey" FOREIGN KEY ("B") REFERENCES "Owner"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CommitteeToOwner" ADD CONSTRAINT "_CommitteeToOwner_A_fkey" FOREIGN KEY ("A") REFERENCES "Committee"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CommitteeToOwner" ADD CONSTRAINT "_CommitteeToOwner_B_fkey" FOREIGN KEY ("B") REFERENCES "Owner"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "_TaskGraph" ADD CONSTRAINT "_TaskGraph_A_fkey" FOREIGN KEY ("A") REFERENCES "Task"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_TaskGraph" ADD CONSTRAINT "_TaskGraph_B_fkey" FOREIGN KEY ("B") REFERENCES "Task"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BudgetToOwner" ADD CONSTRAINT "_BudgetToOwner_A_fkey" FOREIGN KEY ("A") REFERENCES "Budget"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BudgetToOwner" ADD CONSTRAINT "_BudgetToOwner_B_fkey" FOREIGN KEY ("B") REFERENCES "Owner"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_FeeToOwner" ADD CONSTRAINT "_FeeToOwner_A_fkey" FOREIGN KEY ("A") REFERENCES "Fee"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_FeeToOwner" ADD CONSTRAINT "_FeeToOwner_B_fkey" FOREIGN KEY ("B") REFERENCES "Owner"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_SpecialAssessmentToUnit" ADD CONSTRAINT "_SpecialAssessmentToUnit_A_fkey" FOREIGN KEY ("A") REFERENCES "SpecialAssessment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
